3d24a97c84497c9f31ae9c5b942ed947dba80e12
tades <omeroglusedatt@gmail.com>
2022-10-28 17:18:12 +0300

    update list-item.js
    edited delete button

--- src/components/list-item.js
+++ src/components/list-item.js
@@ -1,12 +1,24 @@
-import React, {useState} from 'react';
-import {Box, Button, Center, HStack, Image, Text, VStack} from "@chakra-ui/react";
+import React, { useState } from 'react';
+import { Box, Button, Center, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import logo from '../assets/hotel-image.jpeg'
 import ApproveModal from "../modals/approve-modal";
 
-function ListItem({hotelName, hotelVote, _upVote, _downVote, hotelIndex, deleteHotel}) {
-    const [deleteButtonShow, setDeleteButtonShow] = useState(false);
+function ListItem({ hotelName, hotelVote, _upVote, _downVote, hotelIndex, deleteHotel }) {
+    const [deleteButtonShow, setDeleteButtonShow] = useState(true);
     const [deleteItemDetail, setDeleteItemDetail] = useState({});
-    return <>
+    return <Box position={'relative'}>
+        {deleteButtonShow &&
+            <Box w={'20px'} h={'20px'} rounded={'20px'}
+                bg={'red.400'}
+                color={'white'}
+                position={'absolute'}
+                top={'-5px'}
+                right={'-5px'}
+                onClick={() => {
+                    setDeleteItemDetail({ isOpen: true, name: hotelName, index: hotelIndex })
+                }}>
+                <Center fontSize={12} mt={'1px'}>X</Center>
+            </Box>}
         <HStack
             onMouseEnter={() => {
                 setDeleteButtonShow(true)
@@ -18,21 +30,8 @@ function ListItem({hotelName, hotelVote, _upVote, _downVote, hotelIndex, deleteH
             p={1} mt={1}
             border={'1px solid lightgray'}
             rounded={5}
-            _hover={{bg: 'lightgray', transition: '0.5s', cursor: 'pointer'}}
-            position={'relative'}>
-            {deleteButtonShow &&
-                <Box w={'20px'} h={'20px'} rounded={'20px'}
-                     bg={'red.400'}
-                     color={'white'}
-                     position={'absolute'}
-                     top={'-5px'}
-                     right={'-5px'}
-                     onClick={() => {
-                         setDeleteItemDetail({isOpen: true, name: hotelName, index: hotelIndex})
-                     }}>
-                    <Center fontSize={12} mt={'1px'}>X</Center>
-                </Box>}
-            <Image src={logo} w={120} h={120}/>
+            _hover={{ bg: 'lightgray', transition: '0.5s', cursor: 'pointer' }}>
+            <Image src={logo} w={120} h={120} />
             <VStack>
                 <Box w={'100%'} display={'flex'} justifyContent={'start'} flexDirection={'column'}>
                     <Text fontWeight={'bold'}>{hotelName}</Text>
@@ -47,12 +46,12 @@ function ListItem({hotelName, hotelVote, _upVote, _downVote, hotelIndex, deleteH
             </VStack>
         </HStack>
         <ApproveModal isOpen={deleteItemDetail.isOpen}
-                      hotelName={deleteItemDetail.name}
-                      hotelIndex={deleteItemDetail.index}
-                      close={(value) => setDeleteItemDetail({...deleteItemDetail, isOpen: value})}
-                      deleteHotel={(index) => deleteHotel(index)}
+            hotelName={deleteItemDetail.name}
+            hotelIndex={deleteItemDetail.index}
+            close={(value) => setDeleteItemDetail({ ...deleteItemDetail, isOpen: value })}
+            deleteHotel={(index) => deleteHotel(index)}
         />
-    </>
+    </Box>
 }
 
 export default ListItem;
